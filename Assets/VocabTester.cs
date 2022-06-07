cc0d515294b8399997240d66e4e45e5ceed1cd4a
Joshua <69637774+Joshyx@users.noreply.github.com>
2022-06-07 19:38:58 +0200

    Wortvergleiche fertiggestellt

--- Assets/VocabTester.cs
+++ Assets/VocabTester.cs
@@ -11,26 +11,32 @@ Später werd ich sie löschen
 public class VocabTester : MonoBehaviour
 {
     void Start() {
-        
-        VocabList list1 = new VocabList("lol", "LöL");
-        list1.AddWord(new Word("trrfgd", "o,jmhojnmb"));
 
-        VocabList list2 = new VocabList("englisch_unit_4", "Englisch Unit 4");
-        list2.AddWord(new Word("Hallo", "hello"));
-        list2.AddWord(new Word("Frühstück", "breakfast"));
-        list2.AddWord(new Word("Morgen", "morning"));
 
-        VocabList list3 = new VocabList("Franz Unit 3 Part 1");
-        list3.AddWord(new Word("Guten Tag", "bonjour"));
-        list3.AddWord(new Word("Freund", "ami"));
+        // Du kannst mal an den Variablen ein bisschen rumprobieren, also sie ändern, dann Unity starten und schauen, was rauskommt
+        // correct ist das Wort, was gewollt ist und actual ist das Wort, was dann eingegeben wurde
+        string correct = "Hello";
+        string actual = "Hallo";
 
-        VocabListManager.AddList(list1);
-        VocabListManager.AddList(list2);
-        VocabListManager.AddList(list3);
 
-        VocabListManager.SaveLists();
-        
-        //VocabListManager.LoadLists();
-        Debug.Log(JsonConvert.SerializeObject(VocabListManager.GetList("englisch_unit_4"), Formatting.Indented));
+
+        WordComparer.ErrorInfo info = WordComparer.CheckSimiliarity(correct, actual);
+
+        if(info.GetSimiliarity() == WordComparer.ErrorInfo.ErrorType.CORRECT) {
+            Debug.Log("RICHTIG");
+		} else if(info.GetSimiliarity() == WordComparer.ErrorInfo.ErrorType.WRONG_SPECIAL_CHARACTER) {
+            Debug.Log("FALSCHES ZEICHEN:");
+            foreach(int i in info.GetErrors()) {
+                Debug.Log(actual[i] + " (richtig: " + correct[i] + ")");
+			}
+        } else if (info.GetSimiliarity() == WordComparer.ErrorInfo.ErrorType.DIFFERENT_SPELLING) {
+            Debug.Log("FALSCHE Rechtshcreibung:");
+            foreach (int i in info.GetErrors()) {
+                Debug.Log(actual[i] + " (richtig: " + correct[i] + ")");
+            }
+        } else if (info.GetSimiliarity() == WordComparer.ErrorInfo.ErrorType.WRONG) {
+            Debug.Log("GAAANZ Falsch:");
+            Debug.Log(actual + " (richtig: " + correct + ")");
+        }
     }
 }
